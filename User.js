d6211356d5c258426348263483a5aeaf68de4d6e
JayeshSharma20 <jayesh.sharma@infusai.com>
2025-01-02 12:31:03 +0530

    Update User.js

--- User.js
+++ User.js
@@ -1,63 +1,147 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState, Fragment, useCallback } from 'react'
 import axios from 'axios'
-import Table from 'react-bootstrap/Table';
 import Navbars from './Navbars';
+import Table from './Custom/Table';
+import { userRolesList, getTestingOffice, userList } from '../Api/UserApi';
+import Select from './Custom/Select';
+import { useSelector } from 'react-redux';
 
 const User = () => {
 
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  const [options, setOptions] = useState([])
+  const [selectOption, setSelectOption] = useState(null)
+  // console.log(selectOption)
+  const [filterData, setFilterData] = useState([])
+  // console.log(filterData)
+  const token = localStorage.getItem('token');
+  // console.log(token)
+  // console.log(testing)
+  const columns = [
+    {
+      selector: (info) => info.UserName,
+      name: "UserName",
+      grow: 2,
+      sortable: true,
+    },
+    {
+      selector: (info) => info.FirstName,
+      name: "FirstName",
+      grow: 2,
+      sortable: true,
+    },
+    {
+      selector: (info) => info.LastName,
+      name: "LastName",
+      grow: 2,
+      sortable: true,
+    },
+    {
+      selector: (info) => info.Email,
+      name: "Email",
+      grow: 2,
+      sortable: true,
+    },
+    {
+      selector: (info) => info.Phone,
+      name: "Phone",
+      grow: 2,
+      sortable: true,
+    },
+    {
+      name: "Testing Office",
+      grow: 2,
+      selector: (info) => info.TestingOffice?.TestingOfficeName,
+      sortable: true,
+      sortField: "Testing Office",
+    },
+    {
+      name: "Group",
+      grow: 2,
+      selector: (info) => info.UserGroupName,
+      sortable: true,
+      sortField: "UserGroupName",
+    },
+    {
+      name: "Roles",
+      grow: 2,
+      selector: (info) => info.UserRolesName?.RoleName,
+      sortable: true,
+      sortField: "Roles",
+    },
+  ]
 
-    useEffect(()=>{
-        axios.get('http://dummyjson.com/users')
-        .then(response=>{
-           setData(response.data.users)
-           console.log(response)
-    })
-            // setData(response))
-        .catch(error =>
-          console.log(error)
-          // setLoading(false)        
-        )
-    },[])
+  const callData = useCallback(async () => {
+    await userList(
+      token,
+      `?$count=true&$expand=UserRolesName($select=RoleName),TestingOffice($select=TestingOfficeName)`
+    )
+      .then((data) => {
+        // console.log(data)
+        setData(data?.value)
+        setFilterData(data?.value)
+      })
+      .catch((error) => console.log(error))
+  }, [])
+
+  async function getTestingOfficeOptions() {
+    await getTestingOffice(
+      token,
+      `?$select=TestingOfficeName,Id,isActive`
+    )
+      .then((data) => {
+        // console.log(data)
+        setOptions(data?.value.map((role) => ({ value: role.RoleName, label: role.RoleName })));
+      })
+      .catch((error) => console.log(error))
+  }
+
+  async function getUserRole() {
+    await userRolesList(token)
+      .then((data) => {
+        setOptions(data?.value);
+        // console.log(data?.value)
+      })
+      .catch((error) => console.log(error));
+  }
+
+  useEffect(() => {
+    callData()
+    // getTestingOfficeOptions()
+    getUserRole()
+
+  }, [])
+
+  useEffect(() => {
+    if (selectOption) {
+      const filtered = data.filter((user) => user.UserRolesName?.RoleName === selectOption);
+      // console.log(filtered)
+      setFilterData(filtered);
+    } else {
+      setFilterData(data);
+    }
+  }, [selectOption, data]);
+
 
   return (
-    <>
-    <Navbars/>
-    <div className=' bg-slate-400 flex justify-center items-center '>
-    <div  className='font-bold flex justify-center items-center text-black'>
-    </div>
-      <div className='w-75 rounded flex justify-center items-center bg-white border shadow p-4'>
-      <Table responsive="sm">
-      <thead>   
-          <tr>
-            <th>ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Gender</th>
-            <th>Age</th> 
-          </tr>
-        </thead>
-        <tbody>
-        { 
-          data.map((d,i)=>(
-            <tr key={i}>
-              <td>{d.id}</td>
-              <td>{d.firstName}</td>
-              <td>{d.lastName}</td>
-              <td>{d.email}</td>
-              <td>{d.phone}</td>
-              <td>{d.gender}</td>
-              <td>{d.age}</td>
-            </tr>
-          ))
-        }
-        </tbody>
-        </Table>
+    <Fragment>
+      <Navbars />
+      <Select
+        value={options}
+        selectOption={selectOption}
+        onChange={(e) => {
+          setSelectOption(e.target.value)
+        }}
+      />
+      <div className='flex justify-center items-center text-black'>
+        <div className='w-75 rounded flex justify-center items-center bg-white border shadow p-4'>
+          <Table
+            data={filterData}
+            columns={columns}
+          />
+        </div>
       </div>
-    </div>
-    </>
+    </Fragment>
   )
 }
 
